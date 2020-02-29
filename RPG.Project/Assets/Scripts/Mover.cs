d1cfd8c4bb9c6fd52b2301a4936e1e541c73f605
Hermann <hmcenhill@gmail.com>
2020-02-29 04:57:46 -0500

    11

---
+++ RPG.Project/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform Target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.destination = Target.position;
+
+        
+    }
+}
