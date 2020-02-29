a5e6c25557e5ab069380cb7c4a6fa26a35d62035
Hermann <hmcenhill@gmail.com>
2020-02-29 07:57:38 -0500

    end of section 3 (lecture 19)

---
+++ RPG.Project/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform Target;
+
+    void LateUpdate()
+    {
+        this.transform.position = Target.position;
+    }
+}
---
+++ RPG.Project/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    private Vector3 _target;
+    private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+
+    void Start()
+    {
+        _target = this.transform.position;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    Ray lastRay;
+
+    void Update()
+    {
+        MoveCharacter();
+        AnimateCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToTarget();
+        }
+    }
+
+    private void AnimateCharacter()
+    {
+        var globalVelocity = _navMeshAgent.velocity;
+        var localVelocity = transform.InverseTransformDirection(globalVelocity);
+        _animator.SetFloat("forwardSpeed", localVelocity.z);
+    }
+
+    private void MoveToTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
+        {
+            _target = hit.point;
+            _navMeshAgent.destination = _target;
+        }
+    }
+}
