e4a94bea4a8501778ea988a1163526c833818bc1
Hermann <hmcenhill@gmail.com>
2020-02-29 05:12:41 -0500

    12

--- RPG.Project/Assets/Scripts/Mover.cs
+++ RPG.Project/Assets/Scripts/Mover.cs
@@ -6,11 +6,13 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform Target;
+    [SerializeField] int Speed = 10;
 
     // Update is called once per frame
     void Update()
     {
         var navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = Speed;
         navMeshAgent.destination = Target.position;
 
         
